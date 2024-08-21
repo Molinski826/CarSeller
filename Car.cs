73e566547a59b993a1a3d649238cc44be65595b5
Joey Molinski <molinski826@msn.com>
2024-08-21 14:47:37 -0400

    first commit

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSeller
+{
+    internal class Car
+    {
+        //properties
+        public string Make {  get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        //constructor
+        public Car(string make, string model, int year, decimal price)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+            Price = 0;
+        }
+        public override string ToString()
+        {
+            return $"{Make}, {Model}, {Year}, {Price}";
+        }
+    }
+}
---
+++ UsedCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSeller
+{
+    internal class UsedCar:Car
+    {
+        public double Milage { get; set; }
+        public UsedCar(string make, string model, int year, decimal price, double milage) : base(make, model, year, price)
+        {
+            Milage = milage;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+
+        }
+
+    }
+}
